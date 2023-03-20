bb606088f7f34a5fa3a18f0723a7bcf13659a5aa
Ari Rismansyah <rismansyah.ari48@gmail.com>
2023-03-20 11:46:00 +0700

    merge-landing

--- src/components/landing/LandingPage.vue
+++ src/components/landing/LandingPage.vue
@@ -1,28 +1,169 @@
 <template>
-  <body>
-    <div class="bg-container view">
-      <img class="img-bg" src="@/assets/images/footage/img_(8).jpg" />
-      <div class="mask rgba-blue3-light"></div>
-      <div class="header-bg justify-content-center text-center">
-        <div class="row justify-content-center text-center">
-          <div class="col-lg-8 col-xl-7 col-xxl-6 mx-auto">
-            <h1 class="display-1 text-white fs-60 mb-4 px-md-15 px-lg-0">
-              We bring solutions to make life
-              <span class="underline-3 style-2 yellow">easier</span>
-            </h1>
-            <p class="lead fs-24 text-white lh-sm mb-7 mx-md-13 mx-lg-10">
-              We are a creative company that focuses on long term relationships
-              with customers.
-            </p>
-            <div>
-              <a class="btn btn-white rounded mb-10 mb-xxl-5">Read More</a>
+  <body v-bind:class="themeStore.light ? 'light-mode' : 'dark-mode'">
+    <div class="page">
+      <div class="page-main">
+        <div class="bg-container view">
+          <img class="img-bg" src="@/assets/images/footage/img_(8).jpg" />
+          <div class="mask-bg">
+            <div class="row"></div>
+          </div>
+          <div class="header-bg justify-content-center text-center">
+            <div class="row justify-content-center text-center mt-0">
+              <div class="col-lg-8 col-xl-7 col-xxl-6 mx-auto">
+                <h1 class="display-1 text-white fs-60 mb-4 px-md-15 px-lg-0">
+                  Desa Bumi
+                  <span class="underline-3 style-2 yellow">Sriwijaya</span>
+                </h1>
+
+                <div
+                  class="row justify-content-center"
+                  data-cue="slideInDown"
+                  data-group="page-title"
+                  data-show="true"
+                  style="
+                    animation-name: slideInDown;
+                    animation-duration: 700ms;
+                    animation-timing-function: ease;
+                    animation-delay: 600ms;
+                    animation-direction: normal;
+                    animation-fill-mode: both;
+                  "
+                >
+                  <div class="col-lg-12">
+                    <div class="row justify-content-center">
+                      <figure
+                        class="img-responsive overlay overlay-1 hover-scale mb-0 p-6"
+                      >
+                        <a>
+                          <img
+                            class="img-responsive"
+                            src="@/assets/images/prov-logo/logo1600.png"
+                            alt=""
+                            style="
+                              max-height: 150px;
+                              width: auto;
+                            " /><span></span
+                        ></a>
+                        <figcaption>
+                          <h6 class="from-top mt-8">Kunjungi</h6>
+                        </figcaption>
+                      </figure>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row justify-content-center">
+                  <div class="col-lg-6 col-md-6">
+                    <div class="input-group">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Search for..."
+                        v-model="propsShadow.keyword"
+                        v-on:keyup.enter="loadSearchWilayah"
+                      />
+                      <span
+                        class="input-group-text btn btn-info"
+                        @click="loadSearchWilayah"
+                        ><i class="fe fe-search"></i
+                      ></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- /column -->
             </div>
           </div>
-          <!-- /column -->
+          <div class="overflow-hidden divider-bg">
+            <div class="divider text-light mx-n2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 60">
+                <path
+                  fill="currentColor"
+                  d="M0,0V60H1440V0A5771,5771,0,0,1,0,0Z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="main-content"
+          style="position: absolute; top: 65%; z-index: 5; right: 0; left: 0"
+        >
+          <div class="row mx-1">
+            <!-- unit logo it -->
+            <UnitLogo
+              v-for="(data, idx) in state.kabKota"
+              :key="data.kode_kab"
+              :kodeWilayah="data.kode_prov + data.kode_kab"
+              :name="data.nama"
+            >
+            </UnitLogo>
+          </div>
         </div>
       </div>
     </div>
   </body>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  ref,
+  reactive,
+  onMounted,
+  inject,
+  computed,
+  defineAsyncComponent,
+} from "vue";
+import axios from "axios";
+import type { Wilayah } from "@/models/Wilayah";
+import { useThemeStore } from "@/stores/theme";
+import UnitLogo from "../landing/UnitLogo.vue";
+const themeStore = useThemeStore();
+
+const urlApi = inject("urlApi");
+let props = defineProps({
+  keyword: { type: String },
+});
+let propsShadow = Object.assign({}, props);
+const state = reactive({
+  wilayahs: [] as Wilayah[],
+  kabKota: [],
+});
+
+const loadingState = ref("false");
+const isLoaded = computed(() => loadingState.value === "success");
+
+async function loadSearchWilayah() {
+  loadingState.value = "false";
+  await axios
+    .post(`${urlApi}wilayah/search`, {
+      keyword: propsShadow.keyword,
+    })
+    .then(({ data }) => {
+      if (data.status == "success") {
+        state.wilayahs = data.datas;
+        loadingState.value = "success";
+      } else {
+        console.log("Data gagal disimpan, silahkan ulangi lagi");
+      }
+    });
+}
+
+async function getKabKot() {
+  loadingState.value = "false";
+
+  await axios
+    .get(`${urlApi}wilayah/16`)
+    .then(({ data }) => {
+      state.kabKota = data.datas;
+    })
+    .catch(({ response }) => {
+      console.error(response);
+    });
+}
+
+onMounted(() => {
+  getKabKot();
+});
+</script>
--- src/components/landing/UnitLogo.vue
+++ src/components/landing/UnitLogo.vue
@@ -1,4 +1,41 @@
 <template>
-  <div></div>
+  <div class="col m-0">
+    <div class="card">
+      <div class="card-body px-3 py-3">
+        <figure
+          class="img-responsive overlay overlay-1 hover-scale rounded mb-0"
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          title=""
+          :data-bs-original-title="name"
+        >
+          <a :href="'/monograph/' + kodeWilayah">
+            <img
+              class="img-responsive"
+              :src="getLogoAssets(kodeWilayah)"
+              alt=""
+              style="max-height: 80px; width: auto"
+            />
+            <span class="bg"></span>
+          </a>
+          <figcaption>
+            <small class="mdi md-map from-top"></small>
+          </figcaption>
+        </figure>
+      </div>
+      <!--/.card-body -->
+    </div>
+  </div>
 </template>
-<script setup></script>
+<script setup lang="ts">
+defineProps({
+  kodeWilayah: { type: String },
+  name: { type: String },
+});
+function getLogoAssets(kode: string) {
+  return new URL(
+    `../../assets/images/kabs-logo/logo${kode}.png`,
+    import.meta.url
+  ).href;
+}
+</script>
